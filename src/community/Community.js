b56873aefb1555cfadf2e715dea172c8b74066e5
Sean Perkins <sean@mobility-labs.com>
2016-09-03 15:08:20 -0500

    Fix top margin

--- src/community/Community.js
+++ src/community/Community.js
@@ -5,7 +5,7 @@ import CommunityFeed from './CommunityFeed';
 class Community extends Component {
   render() {
     return (
-      <div>
+      <div className="community-page">
         <h1>Community</h1>
         <p>Here is some subcopy about how great our community is.</p>
         <CommunityFeed />
