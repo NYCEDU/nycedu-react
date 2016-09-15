c617ce49a8a6d3fed2051e0d78f6d1437afe47b4
Sean Perkins <sean@mobility-labs.com>
2016-09-15 18:35:15 -0500

    Add material icons and app drawer

--- src/App.js
+++ src/App.js
@@ -8,11 +8,24 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 import AppBar from 'material-ui/AppBar';
-import FlatButton from 'material-ui/FlatButton';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import theme from './theme';
 
 
 class App extends Component {
+
+  state = {
+    open: false
+  }
+
+  handleToggle(){
+    this.setState({open: !this.state.open});
+  }
+
+  handleClose(){
+    this.setState({open: false});
+  }
 
   render() {
     return (
@@ -23,9 +36,18 @@ class App extends Component {
                             className="home-button">
                               {this.props.children.props.route.title + ' - #nycedu'}
                           </a>}
-                  iconElementRight={<FlatButton label="About" href="/about"/>}
-                  style={{position:'fixed', top: 0}}
-          />
+                  onLeftIconButtonTouchTap={this.handleToggle.bind(this)}
+                  style={{position:'fixed', top: 0}} />
+          <Drawer
+            docked={false}
+            width={200}
+            open={this.state.open}
+            onRequestChange={(open) => this.setState({open})} >
+            <MenuItem leftIcon={<i className="material-icons">face</i>} href="/community">Community</MenuItem>
+            <MenuItem leftIcon={<i className="material-icons">work</i>} href="/projects">Projects</MenuItem>
+            <MenuItem leftIcon={<i className="material-icons">event</i>} href="/events">Events</MenuItem>
+            <MenuItem leftIcon={<i className="material-icons">account_balance</i>} href="/organizations">Organizations</MenuItem>
+          </Drawer>
           {this.props.children}
         </div>
       </MuiThemeProvider>
