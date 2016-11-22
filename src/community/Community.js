cf57398331bc53da463213ed7b98ca5ac19e0687
Rick Metzger <rickymetz@gmail.com>
2016-11-22 11:41:50 -0500

    Create community members cards from an array to reduce repeating code

--- src/community/Community.js
+++ src/community/Community.js
@@ -10,8 +10,49 @@ import sean from './sean.png';
 
 import DocumentTitle from 'react-document-title';
 
-class Community extends Component {
+export default class Community extends Component {
   render() {
+    const people = [
+      {
+        name: "Deborah Chang",
+        imageUrl: deborah,
+        buildCommunity: "I founded #NYCEDU to connect everyone with everyone in NYC education.",
+        getInvolved: <p>Subscribe to the newsletter to learn more.</p>
+      },
+      {
+        name: "Eugene Leventhal",
+        imageUrl: eugene,
+        buildCommunity: "I started the Bronx Education Meetup group to help connect those involved in education in the Bronx. I’m also working on EduDAO, a blockchain educational funding project.",
+        getInvolved: <p>Sign up for the <a href="https://www.meetup.com/Bronx-Education-Meetup/">meetup</a> or email me at <a href="mailto:eugene.leventhal@gmail.com">eugene.leventhal@gmail.com</a>.</p>
+      },
+      {
+        name: "Sean Perkins",
+        imageUrl: sean,
+        buildCommunity: "I started the Bronx Education Meetup group to help connect those involved in education in the Bronx. I’m also working on EduDAO, a blockchain educational funding project.",
+        getInvolved: <p>Sign up for the meetup or email me at eugene.leventhal@gmail.com.</p>
+      },
+    ];
+    const communityMembers = people.map((person) => {
+        return (
+          <Card className="card">
+            <CardMedia>
+              <img src={person.imageUrl} />
+            </CardMedia>
+            <CardTitle title={person.name} />
+            <CardText>
+              <section>
+                <h3>How you’re building community in NYC education:</h3>
+                <p>{person.buildCommunity}</p>
+              </section>
+              <section>
+                <h3>How to get involved:</h3>
+                {person.getInvolved}
+              </section>
+            </CardText>
+          </Card>
+        )
+      }
+    );
     return (
       <div className="community-page container">
         <DocumentTitle title="Our community | #NYCEDU" />
@@ -19,62 +60,13 @@ class Community extends Component {
         <h1>Community</h1>
         <p>If you care about education in NYC, then you are already a member of #NYCEDU! Meet some of your fellow community members.</p>
         <div className="member-container">
-          <Card className="card">
-            <CardMedia>
-              <img src={deborah} />
-            </CardMedia>
-            <CardTitle title="Deborah Chang" />
-            <CardText>
-              <section>
-                <h3>How you’re building community in NYC education:</h3>
-                <p>I founded #NYCEDU to connect everyone with everyone in NYC education.</p>
-              </section>
-              <section>
-                <h3>How to get involved:</h3>
-                <p>Subscribe to the newsletter to learn more.</p>
-              </section>
-            </CardText>
-          </Card>
-          <Card className="card">
-            <CardMedia>
-              <img src={eugene} />
-            </CardMedia>
-            <CardTitle title="Eugene Leventhal" />
-            <CardText>
-              <section>
-                <h3>How you’re building community in NYC education:</h3>
-                <p>I started the Bronx Education Meetup group to help connect those involved in education in the Bronx. I’m also working on EduDAO, a blockchain educational funding project.</p>
-              </section>
-              <section>
-                <h3>How to get involved:</h3>
-                <p>Sign up to the <a href="https://www.meetup.com/Bronx-Education-Meetup/">meetup</a> or email me at <a href="mailto:eugene.leventhal@gmail.com">eugene.leventhal@gmail.com</a>. </p>
-              </section>
-            </CardText>
-          </Card>
-          <Card className="card">
-            <CardMedia>
-              <img src={sean} />
-            </CardMedia>
-            <CardTitle title="Sean Perkins" />
-            <CardText>
-              <section>
-                <h3>How you’re building community in NYC education:</h3>
-                <p>I founded #NYCEDU to connect everyone with everyone in NYC education.</p>
-              </section>
-              <section>
-                <h3>How to get involved:</h3>
-                <p>Subscribe to the newsletter to learn more.</p>
-              </section>
-            </CardText>
-          </Card>
+          {communityMembers}
         </div>
         <object id="middle-background" className="community-backgrounds" type="image/svg+xml" data={middleLandscape}>Your browser does not support svgs.</object>
-        <Card style={{padding: "25px", marginBottom: "100px"}}>
+        <Card style={{padding: "25px", margin: "0 auto 100px", maxWidth: "800px"}}>
           <SignupForm handleSubmission={this.props.handleToast} />
         </Card>
       </div>
     );
   }
 }
-
-export default Community;
