57d316861c043cfc48aae1363ed1e714f5dba0e6
Sean Perkins <sean@mobility-labs.com>
2017-03-20 16:03:35 +0700

    Add additional fields to card

--- src/organizations/OrganizationCard.js
+++ src/organizations/OrganizationCard.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
 import './OrganizationCard.css';
 
-class Organization extends Component {
+export default class OrganizationCard extends Component {
 
   render() {
-    return (<div className='organization col-xs-6 col-sm-4 col-lg-3'>
-              <a target="_blank" href={'http://'+this.props.website}>
-                <img src={this.props.image}
-                     alt={this.props.name + ' logo'} />
-                <h2>{this.props.name}</h2>
-              </a>
+    const {organization, toggleExpanded, expandedHandle} = this.props;
+    return (
+      <div className='organization col-xs-6 col-sm-4 col-lg-4'>
+        <img src={'https://twitter.com/' + organization.twitterHandle + '/profile_image?size=original'}
+             alt={organization.name + ' logo'} />
+        <a target="_blank" href={'http://'+organization.website}>
+          <h2>{organization.name}</h2>
+        </a>
+        <div className="row">
+          <div className="col-sm-4 col-xs-12">
+            <p>{organization.targetAudience}</p>
+            <span>Target user</span>
+          </div>
+          <div className="col-sm-4 col-xs-12">
+            <p>Hub</p>
+            <span>Network</span>
+          </div>
+          <div className="col-sm-4 col-xs-12">
+            <p>{organization.organizationType}</p>
+            <span>Legal Structure</span>
+          </div>
+        </div>
+        <a className="btn btn-primary" target="_blank" href={'http://twitter.com/' + organization.twitterHandle}>
+          Twitter
+        </a>
+        {organization.jobWebsite && (
+          <a className="btn btn-primary" target="_blank" href={organization.jobWebsite}>
+            Jobs Page
+          </a>
+        )}
+        <span onClick={toggleExpanded.bind(null, organization.twitterHandle)}>
+          Expand
+        </span>
+        {expandedHandle === organization.twitterHandle && (
+          <div>
+            {organization.address && <p>{organization.address}</p>}
+            {organization.staticDescription && <p>{organization.staticDescription}</p>}
+            <div className="row">
+              <div className="col-sm-3 col-xs-6">
+                <p>{organization.hq}</p>
+                <span>HQ</span>
+              </div>
+              <div className="col-sm-3 col-xs-6">
+                <p>{organization.numberOfEmployees}</p>
+                <span>NYC Employees</span>
+              </div>
+              <div className="col-sm-3 col-xs-6">
+                <p>{organization.founded}</p>
+                <span>Founded</span>
+              </div>
+              <div className="col-sm-3 col-xs-6">
+                <p>{organization.hq}</p>
+                <span>Impact Area</span>
+              </div>
             </div>
+          </div>
+        )}
+      </div>
     );
   }
 }
-
-export default Organization;
--- src/organizations/OrganizationFeed.js
+++ src/organizations/OrganizationFeed.js
@@ -2,58 +2,67 @@ import React, { Component } from 'react';
 import './OrganizationFeed.css';
 import OrganizationCard from './OrganizationCard';
 
-class OrganizationFeed extends Component {
+export default class OrganizationFeed extends Component {
 
   state = {
-    organizations: []
+    organizations: [],
+    expandedHandle: null,
   }
 
   componentDidMount() {
-    fetch('https://spreadsheets.google.com/feeds/list/17nmBELMdwT1ScV3j5Vt66wIzSphQhnhhQLdBJdP4oN8/od6/public/values?alt=json')
-    .then( (response) => {
-      return response.json() })   
-      .then( (json) => {
-        let orgs = json.feed.entry.map(function(obj){
-          return {
-            address               : obj['gsx$address']['$t'],
-            staticDescription     : obj['gsx$briefdescriptionstatic']['$t'],
-            twitterDescription    : obj['gsx$briefdescriptionviatwitter']['$t'],
-            companyororganization : obj['gsx$companyororganization']['$t'],
-            numberOfEmployees     : obj['gsx$est.ofemployeesinnyc']['$t'],
-            eventsCalendar        : obj['gsx$eventscalendar']['$t'],
-            founded               : obj['gsx$founded']['$t'],
-            funding               : obj['gsx$funding']['$t'],
-            hq                    : obj['gsx$hq']['$t'],
-            jobWebsite            : obj['gsx$jobwebsite']['$t'],
-            latitude              : obj['gsx$latitude']['$t'],
-            longitude             : obj['gsx$longitude']['$t'],
-            miscNotes             : obj['gsx$miscnotes']['$t'],
-            organizationType      : obj['gsx$organizationtype']['$t'],
-            targetAudience        : obj['gsx$targetaudience']['$t'],
-            twitterHandle         : obj['gsx$twitterhandle']['$t'],
-            websiteUrl            : obj['gsx$websiteurl']['$t']}
-        });
-        this.setState({organizations: orgs});
-      });
+    this.getOrganizations();
+  }
+
+  getOrganizations = async () => {
+    const response = await fetch('https://spreadsheets.google.com/feeds/list/17nmBELMdwT1ScV3j5Vt66wIzSphQhnhhQLdBJdP4oN8/od6/public/values?alt=json');
+    const json = await response.json() 
+    let orgs = json.feed.entry.map((obj) => {
+      return {
+        address               : obj['gsx$address']['$t'],
+        staticDescription     : obj['gsx$briefdescriptionstatic']['$t'],
+        twitterDescription    : obj['gsx$briefdescriptionviatwitter']['$t'],
+        name                  : obj['gsx$companyororganization']['$t'],
+        numberOfEmployees     : obj['gsx$est.ofemployeesinnyc']['$t'],
+        eventsCalendar        : obj['gsx$eventscalendar']['$t'],
+        founded               : obj['gsx$founded']['$t'],
+        funding               : obj['gsx$funding']['$t'],
+        hq                    : obj['gsx$hq']['$t'],
+        jobWebsite            : obj['gsx$jobwebsite']['$t'],
+        latitude              : obj['gsx$latitude']['$t'],
+        longitude             : obj['gsx$longitude']['$t'],
+        miscNotes             : obj['gsx$miscnotes']['$t'],
+        organizationType      : obj['gsx$organizationtype']['$t'],
+        targetAudience        : obj['gsx$targetaudience']['$t'],
+        twitterHandle         : obj['gsx$twitterhandle']['$t'],
+        websiteUrl            : obj['gsx$websiteurl']['$t']}
+      }
+    );
+    this.setState({organizations: orgs});
+  }
+
+  toggleExpanded = (expandedHandle) => {
+    this.setState({expandedHandle});
   }
 
   render() {
+    const {organizations, expandedHandle} = this.state;
     return (
       <ul className="organization-list row">
-        {this.state.organizations.map(
-          function(entry, i){
-            return <OrganizationCard
-                      key={i}
-                      image={'https://twitter.com/'+entry.twitterHandle+'/profile_image?size=original'}
-                      twitterHandle={entry.twitterHandle}
-                      name={entry.companyororganization}
-                      website={entry.websiteUrl}
-                      type={entry.organizationType} />
+        {organizations.map(
+          (organization) => {
+            if (organization.twitterHandle) {
+              return (
+                <OrganizationCard
+                  expandedHandle={expandedHandle}
+                  key={organization.twitterHandle}
+                  organization={organization}
+                  toggleExpanded={this.toggleExpanded}
+                />
+              )
+            }
           }
         )}
       </ul>
     );
   }
 }
-
-export default OrganizationFeed;
