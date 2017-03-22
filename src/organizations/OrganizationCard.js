dee9928ec728ae63621a4abd3a34af880e5ce83a
Rick Metzger <rickymetz@gmail.com>
2017-03-22 18:00:11 +0700

    Add organization card styles

--- src/organizations/OrganizationCard.js
+++ src/organizations/OrganizationCard.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
 import './OrganizationCard.css';
 
 export default class OrganizationCard extends Component {
@@ -7,61 +10,106 @@ export default class OrganizationCard extends Component {
     const {organization, toggleExpanded, expandedHandle} = this.props;
     return (
       <div className='organization col-xs-6 col-sm-4 col-lg-4'>
-        <img src={'https://twitter.com/' + organization.twitterHandle + '/profile_image?size=original'}
-             alt={organization.name + ' logo'} />
-        <a target="_blank" href={'http://'+organization.website}>
-          <h2>{organization.name}</h2>
-        </a>
-        <h3>{organization.topic}</h3>
-        <div className="row">
-          <div className="col-sm-4 col-xs-12">
-            <p>{organization.targetAudience}</p>
-            <span>Target user</span>
-          </div>
-          <div className="col-sm-4 col-xs-12">
-            <p>{organization.network}</p>
-            <span>Network</span>
-          </div>
-          <div className="col-sm-4 col-xs-12">
-            <p>{organization.organizationType}</p>
-            <span>Legal Structure</span>
-          </div>
-        </div>
-        <a className="btn btn-primary" target="_blank" href={'http://twitter.com/' + organization.twitterHandle}>
-          Twitter
-        </a>
-        {organization.jobWebsite && (
-          <a className="btn btn-primary" target="_blank" href={organization.jobWebsite}>
-            Jobs Page
-          </a>
-        )}
-        <span onClick={toggleExpanded.bind(null, organization.twitterHandle)}>
-          Expand
-        </span>
-        {expandedHandle === organization.twitterHandle && (
-          <div>
-            {organization.address && <p>{organization.address}</p>}
-            {organization.staticDescription && <p>{organization.staticDescription}</p>}
-            <div className="row">
-              <div className="col-sm-3 col-xs-6">
-                <p>{organization.hq}</p>
-                <span>HQ</span>
-              </div>
-              <div className="col-sm-3 col-xs-6">
-                <p>{organization.numberOfEmployees}</p>
-                <span>NYC Employees</span>
-              </div>
-              <div className="col-sm-3 col-xs-6">
-                <p>{organization.founded}</p>
-                <span>Founded</span>
-              </div>
-              <div className="col-sm-3 col-xs-6">
-                <p>{organization.impactArea}</p>
-                <span>Impact Area</span>
-              </div>
+        <Card>
+          <CardMedia
+            style={{
+              height: '250px',
+              background:'radial-gradient(rgba(0, 0, 0, 0), rgba(104, 159, 56, 0.15)), url(https://twitter.com/' + organization.twitterHandle + '/profile_image?size=original)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+            className="organization-img"
+          >
+          </CardMedia>
+          {organization.websiteUrl ? (
+            <a target="_blank" href={'http://'+organization.websiteUrl} className="organization-link">
+              <CardTitle
+                title={organization.name}
+                subtitle={organization.topic}
+                titleColor='#8BC34A'
+              />
+            </a>
+          ) : (
+            <CardTitle
+              title={organization.name}
+              subtitle={organization.topic}
+            />
+          )}
+         <CardText>
+            <div className="organization-meta">
+              {organization.targetAudience &&(
+                <div className="organization-meta-item">
+                  <p>{organization.targetAudience}</p>
+                  <label>Target user</label>
+                </div>
+              )}
+              {organization.network &&(
+                <div className="organization-meta-item">
+                  <p>{organization.network}</p>
+                  <label>Network</label>
+                </div>
+              )}
+              {organization.organizationType &&(
+                <div className="organization-meta-item">
+                  <p>{organization.organizationType}</p>
+                  <label>Legal Structure</label>
+                </div>
+              )}
             </div>
-          </div>
-        )}
+          </CardText>
+            <CardActions
+              showExpandableButton={
+                organization.hq || organization.numberOfEmployees || organization.founded || organization.impactArea ?
+                true
+                :
+                false}
+            >
+            <FlatButton
+              label="Twitter"
+              href={'http://twitter.com/' + organization.twitterHandle}
+              target="_blank"
+            />
+            {organization.jobWebsite && organization.jobWebsite != "N/A" && (
+              <FlatButton
+                label="Jobs Page"
+                href={organization.jobWebsite}
+                target="_blank"
+              />
+            )}
+          </CardActions>
+           <CardText expandable={true} actAsExpander={true}>
+            {organization.address && <p className="organization-address">{organization.address}</p>}
+            {organization.staticDescription &&
+              <p className="organization-description">{organization.staticDescription}</p>
+            }
+            <div className="organization-meta">
+              {organization.hq && (
+                <div className="organization-meta-item">
+                  <p>{organization.hq}</p>
+                  <label>HQ</label>
+                </div>
+              )}
+              {organization.numberOfEmployees && (
+                <div className="organization-meta-item">
+                  <p>{organization.numberOfEmployees}</p>
+                  <label>NYC Employees</label>
+                </div>
+              )}
+              {organization.founded && (
+                <div className="organization-meta-item">
+                  <p>{organization.founded}</p>
+                  <label>Founded</label>
+                </div>
+              )}
+              {organization.impactArea && (
+                <div className="organization-meta-item">
+                  <p>{organization.impactArea}</p>
+                  <label>Impact Area</label>
+                </div>
+              )}
+            </div>
+          </CardText>
+        </Card>
       </div>
     );
   }
