f2c65aba9296b78668ce4a49873aa3721b7c4657
Sean Perkins <sean@mobility-labs.com>
2019-05-29 10:13:57 -0400

    Allow calendar event to be missing fields

--- src/events/CalendarEvent.js
+++ src/events/CalendarEvent.js
@@ -16,20 +16,23 @@ class CalendarEvent extends Component {
     const start = moment(event.start.dateTime).format("h:mma");
     const end = moment(event.end.dateTime).format("h:mma");
     const emailHash = md5(this.props.event.organizer.email).toString();
-    let lines = event.description.split('\n');
-    
     let link =  event.htmlLink;
     const isLink = /(https?:\/\/[^\s]+)/
-    if (lines[lines.length - 1].match(isLink)) {
-      link = lines[lines.length - 1].match(isLink)[0];
-      lines.splice(-1,1); //Remove the link from the description and make it a button
+    let description
+    if (event.description) {
+      let lines = event.description.split('\n');
+
+      if (lines[lines.length - 1].match(isLink)) {
+        link = lines[lines.length - 1].match(isLink)[0];
+        lines.splice(-1,1); //Remove the link from the description and make it a button
+      }
+
+      description = lines.map((line, i) => {
+        return <p key={i}>{line}</p>;
+      });
     }
-    
-    const description = lines.map((line, i) => {
-      return <p key={i}>{line}</p>;
-    });
-    
-    
+
+
     return(
       <div className="row">
         <Card className="event col-md-12">
@@ -40,13 +43,22 @@ class CalendarEvent extends Component {
                       actAsExpander={true}
                       showExpandableButton={true} />
           <CardText className="event-body" expandable={true}>
-            <div className="event-location">
-              <span>Where:</span> {event.location}
-            </div>
-            <p className="event-description">{description}</p>
-            <p>Organizer: <a href={'mailto:'+event.organizer.email}>
-              {event.organizer.displayName}</a>
-            </p>
+            {
+              event.location &&
+              <div className="event-location">
+                <span>Where:</span> {event.location}
+              </div>
+            }
+            {
+              description &&
+              <p className="event-description">{description}</p>
+            }
+            {
+              event.organizer.email && event.organizer.displayName &&
+              <p>Organizer: <a href={'mailto:'+event.organizer.email}>
+                {event.organizer.displayName}</a>
+              </p>
+            }
             <RaisedButton label="learn more" primary={true} target="_blank" href={link} />
           </CardText>
         </Card>
@@ -56,4 +68,3 @@ class CalendarEvent extends Component {
 }
 
 export default CalendarEvent;
-
